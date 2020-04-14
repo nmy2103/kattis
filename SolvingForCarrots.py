5f9bd5b31f31556514ada3bb0c38a4dbad0a7261
Natasha Yamane <60267667+nmy2103@users.noreply.github.com>
2020-04-14 11:32:27 -0400

    Add files via upload

---
+++ SolvingForCarrots.py
@@ -0,0 +1,10 @@
+# Natasha M. Yamane
+# Kattis: Solving for Carrots
+
+# April 14, 2020
+
+
+contestants = input('Enter the # of contestants and problems solved: ').split()
+for c in range(int(contestants[0])):
+    input('Describe yourself: ')
+print(contestants[1])
